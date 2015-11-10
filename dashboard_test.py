d28df39cd4dab267e890ff6ec95dbaae00fa2251
Yegor Silyutin <ysilyutin@gmail.com>
2015-11-09 22:59:31 -0800

    Using the TestSuites feature of unittest

--- dashboard_test.py
+++ dashboard_test.py
@@ -2,7 +2,7 @@ import unittest
 from selenium import webdriver
 
 
-class SmokeTest(unittest.TestCase):
+class DashboardTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
@@ -15,26 +15,22 @@ class SmokeTest(unittest.TestCase):
 
     def test_verify_amount_of_testruns(self):
         # Get the email and password textboxes
-        self.email_field = self.driver.find_element_by_name("email")
-        self.email_field.clear()
+        email_field = self.driver.find_element_by_name("email")
+        email_field.clear()
 
-        self.password_field = self.driver.find_element_by_name("password")
-        self.password_field.clear()
+        password_field = self.driver.find_element_by_name("password")
+        password_field.clear()
 
-        self.sign_in_button = self.driver.find_element_by_id("log-in")
+        sign_in_button = self.driver.find_element_by_id("log-in")
 
         # Enter valid email\password and click Sign In button
-        self.email_field.send_keys("*****@testmunk.com")
-        self.password_field.send_keys("****")
-        self.sign_in_button.click()
+        email_field.send_keys("*****@testmunk.com")
+        password_field.send_keys("****")
+        sign_in_button.click()
 
         # Find amount of testruns on the page
         testruns = self.driver.find_elements_by_class_name("run-name-input")
-
-        print("Found " + str(len(testruns)) + " testruns.")
-
-        for testrun in testruns:
-            print(testrun.text)
+        self.assertEqual(len(testruns), 33)
 
     @classmethod
     def tearDownClass(cls):
